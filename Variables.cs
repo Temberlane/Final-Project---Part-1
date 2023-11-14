26f98a1271035379bbce7da8a0cb1b07756e99a6
Temberlane <Temberlane195@gmail.com>
2023-11-14 13:23:11 -0500

    C#

---
+++ Variables.cs
@@ -0,0 +1,18 @@
+// C# is a language designed to be like Java and is an object orientated language
+// It has many capabilities such as writing web apps, backend, frontend, mobile apps ect.
+
+
+// First C# Program
+
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}
