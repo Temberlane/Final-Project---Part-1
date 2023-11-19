c00c85deb867d53aa12c796a9302817b78c518d5
Temberlane <Temberlane195@gmail.com>
2023-11-18 20:57:44 -0500

    formatting

--- UnFinishedLessons/Input_Type_FloorDivision_Modulus_Math_Formatting/Program.cs
+++ UnFinishedLessons/Input_Type_FloorDivision_Modulus_Math_Formatting/Program.cs
@@ -35,3 +35,48 @@ Console.WriteLine(6 % 4); // Returns the remainder of 2
 // Math.Round(x) Rounds the number to the nearest whole number
 
 // Formatting Numbers
+// You can format numbers within a string using string interpolation which uses $ before the string to signify string interpolation
+
+string firstName = "John";
+string lastName = "Doe";
+string fullname = $"full name is: {firstName} {lastName}";
+Console.WriteLine(fullname);
+
+// This works with strings aswell
+double unitPrice = 49.99;
+int quantity = 30;
+Console.WriteLine($"Subtotal: ${quantity * unitPrice}");
+
+// Formatted with two decimal places and thousands separator
+Console.WriteLine($"Subtotal: ${quantity * unitPrice:0,0.00}");
+
+// Formatting percent numbers
+double salesTaxRate = 0.065;
+Console.WriteLine($"Sales Tax Rate: {salesTaxRate:P2}");
+
+// Multiline format strings
+// Using '\n' for line break
+string output1 = "hello \n my name is thomas";
+Console.WriteLine(output1);
+
+// Using @ quotation marks for multiline strings
+string output2 = @"
+hello
+my
+name
+is
+Thomas";
+Console.WriteLine(output2);
+
+// Formatting width and alignment
+double subtotal = quantity * unitPrice;
+double salesTax = salesTaxRate * subtotal;
+double total = subtotal + salesTax;
+
+string output3 = $@"
+Sales Tax Rate: {salesTaxRate:P2}
+Subtotal:       ${subtotal,9:0,0.00}  // 9 characters wide, right-aligned, with two decimal places
+Sales Tax:      ${salesTax,9:0,0.00}  // 9 characters wide, right-aligned, with two decimal places
+Total:          ${total,9:0,0.00}      // 9 characters wide, right-aligned, with two decimal places";
+
+Console.WriteLine(output3);
--- UnFinishedLessons/Input_Type_FloorDivision_Modulus_Math_Formatting/obj/Debug/net8.0/Input_Type_FloorDivision_Modulus_Math_Formatting.AssemblyInfo.cs
+++ UnFinishedLessons/Input_Type_FloorDivision_Modulus_Math_Formatting/obj/Debug/net8.0/Input_Type_FloorDivision_Modulus_Math_Formatting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Input_Type_FloorDivision_Modulus_Math_Formatting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2315226c4eb4b982b69af87beb53421b51ab21e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Input_Type_FloorDivision_Modulus_Math_Formatting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Input_Type_FloorDivision_Modulus_Math_Formatting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
