273c636f7cfddffd4b2d725b315cb31933d0bad4
Temberlane <Temberlane195@gmail.com>
2023-11-18 21:46:16 -0500

    logical operators

--- UnFinishedLessons/LogicalOperators_IfStatements/obj/Debug/net8.0/LogicalOperators_IfStatements.AssemblyInfo.cs
+++ UnFinishedLessons/LogicalOperators_IfStatements/obj/Debug/net8.0/LogicalOperators_IfStatements.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogicalOperators_IfStatements")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00c85deb867d53aa12c796a9302817b78c518d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogicalOperators_IfStatements")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogicalOperators_IfStatements")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
