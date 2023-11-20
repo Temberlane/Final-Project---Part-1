ce2e0a9829c6d04d434f04613dcc6a57cce55a97
Temberlane <Temberlane195@gmail.com>
2023-11-20 18:46:33 -0500

    Finished functions program

--- UnFinishedLessons/Functions/obj/Debug/net8.0/Functions.AssemblyInfo.cs
+++ UnFinishedLessons/Functions/obj/Debug/net8.0/Functions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Functions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+908c7a4a1520cf30a892dee83688cc7004920b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Functions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Functions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
