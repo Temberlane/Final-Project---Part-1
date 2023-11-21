8c9eb10ddb779a1c80e67b28d426bd066b6c35b5
Temberlane <Temberlane195@gmail.com>
2023-11-21 13:24:06 -0500

    Collections

--- UnFinishedLessons/Collections_Data/obj/Debug/net8.0/Collections_Data.AssemblyInfo.cs
+++ UnFinishedLessons/Collections_Data/obj/Debug/net8.0/Collections_Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Collections_Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b8fd2750baacee4534519ec55001cd1b04dc51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Collections_Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Collections_Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
